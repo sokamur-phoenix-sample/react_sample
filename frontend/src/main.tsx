dfed6fb5ed0bfcc9c35217c5300a6d8c689b4223
Shinji Okamura <sokamur@newtown-lab>
2026-03-17 15:43:42 +0900

    Added react component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Hello from './components/Hello'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Hello name="React" />
+  </React.StrictMode>
+);
